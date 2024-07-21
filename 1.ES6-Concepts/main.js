34c050194bd78ab0ae437acb3c8aae27bccc2210
Sky Blue <dawadiaakash@gmail.com>
2024-07-21 15:06:34 +0545

    Template Literal

--- 1.ES6-Concepts/main.js
+++ 1.ES6-Concepts/main.js
@@ -32,4 +32,18 @@ function getName(name) {
   console.log(a || b);
 
 
+
+  //===================Template literals============================
+  //Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions, and special constructs called tagged templates.
+//Template literals are sometimes informally called template strings, because they are used most commonly for string interpolation (to create strings by doing substitution of placeholders).
+// However, a tagged template literal may not result in a string; it can be used with a custom tag function to perform whatever operations you want on the different parts of the template literal.
+
+
+let name1 = "Kath";
+let name2 = "Bloom";
+//  console.log(name1 + " " + name2);
+console.log(`${name1} ${name2}`);
+
+
+
   
