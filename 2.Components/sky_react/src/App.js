a4929fc048685ea6b445dee74502963323b4ce48
Sky Blue <dawadiaakash@gmail.com>
2024-07-22 22:23:44 +0545

    JSX-Hello component

--- 2.Components/sky_react/src/App.js
+++ 2.Components/sky_react/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/practice';
 import Welcome from './components/welcome';
+import Hello from './components/hello';
 
 
 function App() {
   return( 
     <div className="App">
-      <Greet></Greet> 
-     <Welcome />
-    </div>
+      {/* <Greet></Greet>  */}
+     {/* <Welcome /> */}
+     <Hello/>
+         </div>
   );
 }
 
