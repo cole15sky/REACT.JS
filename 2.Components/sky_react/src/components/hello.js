161df1e5d1768b9852ff78341ef395992e2c0dc4
Sky Blue <dawadiaakash@gmail.com>
2024-07-22 22:48:12 +0545

    With and without JSX

--- 2.Components/sky_react/src/components/hello.js
+++ 2.Components/sky_react/src/components/hello.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Hello = ()=>{
-    return(
-        <div>
-            <h1> Hey,this is Aakash.</h1>
-        </div>
-    )
+const Hello = () =>{
+//     return(
+//         <div className ='dummyClass>
+//             <h1> Hey,this is Aakash.</h1>
+//         </div>
+//     )
+// 
+return React.createElement('div',
+    {id:'hello', className:'dummyClass'},
+    React.createElement('h1',null,'Hello, this is Aakash')
+)
+
 }
 export default Hello
