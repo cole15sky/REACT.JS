33b5b75c6f222209e983022cd9c6e865b6f64088
Sky Blue <dawadiaakash@gmail.com>
2024-07-22 15:22:16 +0545

    Functional-Components

--- 2.Components/sky_react/src/App.js
+++ 2.Components/sky_react/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/practice';
+import {Greet} from './components/practice';
 
 
 function App() {
--- 2.Components/sky_react/src/components/practice.js
+++ 2.Components/sky_react/src/components/practice.js
@@ -1,6 +1,12 @@
 import React from 'react'
-function Greet(){
-    return <h1> Hello I'm learing React.</h1>
-}
+// function Greet(){
+//     return <h1> Hello I'm learing React.</h1>
+// }
+
+const Greet = () => <h1> Hello I'm learing React </h1>
+
+//NOTE: Here, we are exporting components as default export from practice.js
+//This is what allows us to import the component with any name.
+
 
 export default Greet
