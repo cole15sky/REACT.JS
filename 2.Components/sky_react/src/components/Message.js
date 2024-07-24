b26d1126bb31566bff0fa2197dbf3ed8dd9171fc
Sky Blue <dawadiaakash@gmail.com>
2024-07-24 17:51:59 +0545

    Button handeling

--- 2.Components/sky_react/src/components/Message.js
+++ 2.Components/sky_react/src/components/Message.js
@@ -1,19 +1,23 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
-class Message extends Component{
-    constructor(){
+class Message extends Component {
+    constructor() {
         super()
-        this.state={
-            message:'Welcome Guys.'
+        this.state = {
+            message: 'Welcome Guys.'
         }
     }
-render (){
-    return(
-        <h1>
-          {this.state.message}
-        </h1>
-    )
-}
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.message}
+                </h1>
+                <button> Check Me!</button>
+            </div>
+
+        )
+    }
 }
 
 export default Message
@@ -21,4 +25,10 @@ export default Message
 
 
 
-//step1; create a step objecty and initalize it -> done inside a class constructior
+//step1; create a step objecty and initalize it -> done inside a class constructor.
+// within the constructor we use super() method to extend react component and a call has be made to the base class constructor.
+// step2; create state object 
+// step3; initalize properties and bind the state value in render function.
+// Now we got ability to change the message .
+
+//step4; add html to button element.
