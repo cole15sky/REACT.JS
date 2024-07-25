bcada8a0e875032857212bb9099e76a46d564759
Sky Blue <dawadiaakash@gmail.com>
2024-07-25 16:53:36 +0545

    Nested Components

--- 2.Components/sky_react/src/App.js
+++ 2.Components/sky_react/src/App.js
@@ -4,13 +4,14 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import Productlist from './components/product';
 // import Hello from './components/Hello';
 
 
 function App() {
   return (
     <div className="App"> 
-      <Greet name="Aakash" heroName="Gentleman" />
+      {/* <Greet name="Aakash" heroName="Gentleman" /> */}
         {/* <p> This is children props</p>
         <button> Click me</button>
       </Greet>
@@ -24,6 +25,10 @@ function App() {
        {/* <Message /> */}
       {/* <Hello /> */}
       {/* <Counter /> */}
+
+
+
+      <Productlist/>
     </div>
   );
 }
---
+++ 2.Components/sky_react/src/components/product/index.js
@@ -0,0 +1,12 @@
+
+
+const dummyProductData = ['Product 1','Product 2','Product 3']
+
+function Productlist(){
+    return (<div>
+        <h3>React-JS concepts. </h3>
+    </div>
+    );
+}
+
+export default Productlist;
