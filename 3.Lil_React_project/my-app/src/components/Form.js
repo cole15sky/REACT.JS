b51fe2cb86394b1aa6d9578690da4249813bf940
Sky Blue <dawadiaakash@gmail.com>
2024-07-27 21:12:40 +0545

    Conditional rendering

--- 3.Lil_React_project/my-app/src/components/Form.js
+++ 3.Lil_React_project/my-app/src/components/Form.js
@@ -1,21 +1,36 @@
 import React from 'react'
 
 
-function handleRegister(event){
+function handleRegister(event) {
     event.preventDefault();  //Don't reload the screen on calling function. 
     console.log("Hello Check form");
+
+
 }
 
-function Form(){
-    return(
-        <div>
-            <form>
-            <input type='email' placeholder='Enter your email'></input>
-            <button onClick={handleRegister}>Submit.</button>
+let age = 20;
+let emailId ="cole15sky.com";
 
 
-            </form>
-        </div>
+function Form() {
+    return (
+
+        <form>
+            {/* Conditional Statement */}
+
+            {age > 18 && emailId =='cole15sky'? (
+                <div>
+                    <input type='email' placeholder='Enter your email'></input>
+                    <button onClick={handleRegister}>Submit.</button>
+                </div>
+
+            ) : (
+            <h1> You are kiddo boi.</h1>
+
+        )};
+
+        </form>
+
     )
 }
 
