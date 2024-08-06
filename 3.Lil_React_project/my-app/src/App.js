c27330b35ce94efeef4b45c82b04958259ac4996
Sky Blue <dawadiaakash@gmail.com>
2024-08-06 16:59:04 +0545

    HooksUseState practice

--- 3.Lil_React_project/my-app/src/App.js
+++ 3.Lil_React_project/my-app/src/App.js
@@ -2,14 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
 import UserList from './components/UserList';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter,setCounter] = useState (10)
+  // const counter = 10
+
+  const addValue = ()=> {
+    // counter = counter + 1
+    // setCounter(counter)
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+      setCounter (counter -1)
+    }
   return (
-    <div> 
-      <Form />
-      <UserList />
-      {/* <h1> Hey, there I go..</h1> */}
-    </div>
+    <>
+    <h1> Hi I'm learning Hooks.</h1>
+    <h2> counter values = {counter}.</h2>
+
+    <button onClick={addValue} > Add Value {counter} </button>
+    <br/>
+    <button onClick={removeValue}> Remove Value {counter} </button>
+    <p> footer: {counter} </p>
+    </>
   )
 }
 
